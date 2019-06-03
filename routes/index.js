9d5c5ff26f2a03111d9d4d59798cf35739fd7b7c
Edwin <edshen17@avenues.org>
2019-06-03 10:25:24 +0800

    completed about-us page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 
+// GET /
 router.get('/', function(req, res, next) {
   return res.render('index', {
     title: 'Home'
+  });
+});
+
+// GET About
+router.get('/about-us', function(req, res, next) {
+  res.render('about-us', {
+    title: 'About Us'
   });
 });
