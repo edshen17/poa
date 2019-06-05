79095cabf2c015b3f583ed8798c0c452811e5c15
Edwin <edshen17@avenues.org>
2019-06-05 21:42:45 +0900

    added admissions and infant-program pages

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,23 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// GET About
+// GET about-us
 router.get('/about-us', function(req, res, next) {
   res.render('about-us', {
     title: 'About Us'
+  });
+});
+
+// GET infant-program
+router.get('/infant-program', function(req, res, next) {
+  res.render('infant-program', {
+    title: 'Infant Program'
+  });
+});
+
+// GET infant-program
+router.get('/admissions', function(req, res, next) {
+  res.render('admissions', {
+    title: 'Infant Program'
   });
 });
